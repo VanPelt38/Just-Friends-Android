856706703eafa9f86acf36ec9930919ca1fbc85b
Jake Gordon <jakegordon53@gmail.com>
2024-11-03 23:39:55 +0000

    - added-fab-linking-to-help-centre

--- app/src/main/java/com/example/justfriends/ReusableViews/MainScaffold.kt
+++ app/src/main/java/com/example/justfriends/ReusableViews/MainScaffold.kt
@@ -22,8 +22,16 @@ import androidx.navigation.compose.rememberNavController
 import com.example.justfriends.Navigation.HomeNavHost
 import com.example.justfriends.Utils.DataStoreManager
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Icon
 import androidx.compose.ui.platform.LocalContext
@@ -35,7 +43,10 @@ import com.example.justfriends.Features.SettingsFeature.SettingsViewModel
 import com.example.justfriends.Navigation.FriendsNavHost
 import com.example.justfriends.Navigation.SettingsNavHost
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.google.firebase.auth.FirebaseAuth
 import androidx.compose.runtime.remember
+import com.example.justfriends.Utils.DataStoreKeys
+import com.google.firebase.Firebase
 
 @Composable
 fun MainView(dataStoreManager: DataStoreManager,
@@ -61,6 +72,15 @@ fun MainView(dataStoreManager: DataStoreManager,
     val currentSettingsBackStackEntry by settingsNavController.currentBackStackEntryAsState()
     val canNavigateBackSettings = remember(currentSettingsBackStackEntry) {
         settingsNavController.previousBackStackEntry != null
+    }
+    val context = LocalContext.current
+    var auth: FirebaseAuth = FirebaseAuth.getInstance()
+    var userEmail: String = "Not found"
+    var errorAlertState by remember { mutableStateOf<String?>(null) }
+    var errorAlertStateTitle by remember { mutableStateOf<String?>(null) }
+
+    LaunchedEffect(key1 = Unit) {
+       userEmail = getUserEmail(dataStoreManager)
     }
 
     JustFriendsTheme {
@@ -114,6 +134,34 @@ fun MainView(dataStoreManager: DataStoreManager,
                     colors = TopAppBarDefaults.topAppBarColors(containerColor = Color(red = 19, green = 0, blue = 142))
                 )
             },
+            floatingActionButton = {
+                FloatingActionButton(onClick = {
+
+                    val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
+                        data = Uri.parse("mailto:")
+                        putExtra(Intent.EXTRA_EMAIL, arrayOf("justfriendshelpdesk@gmail.com"))
+                        putExtra(Intent.EXTRA_SUBJECT, "Just Friends Support Issue")
+                        putExtra(Intent.EXTRA_TEXT, "User ID: ${auth.currentUser?.uid}, " +
+                                "Email: ${userEmail}, " +
+                                "Version: ${Build.VERSION.RELEASE}")
+                    }
+
+                    if (emailIntent.resolveActivity(context.packageManager) != null) {
+                        context.startActivity(emailIntent)
+                    } else {
+                        errorAlertStateTitle = "Uh-oh"
+                        errorAlertState = "Looks like your device doesn't have email configured." +
+                                "Please set it up and try again." +
+                                "You can also email us directly at justfriendshelpdesk@gmail.com ."
+                    }
+
+                },
+                    shape = CircleShape,
+                   containerColor = Color(red = 19, green = 0, blue = 142)
+                    ) {
+                    Icon(Icons.Filled.Email, contentDescription = "Add item")
+                }
+            },
             bottomBar = {
                 TabBar(selectedTab) { newIndex ->
                     selectedTab = newIndex
@@ -143,5 +191,28 @@ fun MainView(dataStoreManager: DataStoreManager,
                 }
             }
         )
+
+        errorAlertState?.let { error ->
+            AlertDialog(onDismissRequest = {
+                errorAlertState = null
+                errorAlertStateTitle = null
+            },
+                title = {Text(errorAlertStateTitle ?: "")},
+                text = {Text(errorAlertState ?: "error")},
+                confirmButton = {
+                    Button(
+                        onClick = {
+                            errorAlertState = null
+                            errorAlertStateTitle = null
+                        }
+                    ) {
+                        Text("Okay")
+                    }
+                })
+        }
     }
+}
+
+suspend fun getUserEmail(dataStore: DataStoreManager): String {
+    return dataStore.read(DataStoreKeys.email)
 }
